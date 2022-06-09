c5860059a3347965fcebe55e4e11c03cd6abdf21
Cankat <jankatorumanp@gmail.com>
2022-06-09 14:51:50 +0300

    EDIT script.js, FINISH basic sketching mechanic

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
 const gridDiv = document.querySelector(".grid-div");
 let box = document.createElement("div");
 box.classList.add("box-class");
+// box.addEventListener("mouseenter", function () {
+//   // let r = document.querySelector(":root");
+//   // let hslValue = getComputedStyle(r).getPropertyValue("--hsl-value");
+//   // console.log(hslValue);
+//   // r.style.setProperty("--hsl-value", hslValue - 50);
+//   console.log("HELLO WORLD");
+// });
 
-// let buildGrid = (boxNum) => {
-//   for (let i = 0; i < boxNum; i++) {
-//     box.classList.remove(`box-div-${i}`);
-//     box.classList.add(`box-div-${i + 1}`);
-//     gridDiv.appendChild(box.cloneNode(true)); // use cloneNode to append same item multiple times.
-//   }
-// };
+// function attachEventListener() {
+//   const cells = document.querySelectorAll(".box-class");
+//   cells.forEach((cell) => {
+//     cell.addEventListener("mouseenter", function () {
+//       let r = document.querySelector(":root");
+//       let hslValue = getComputedStyle(r).getPropertyValue("--hsl-value");
+//       console.log(hslValue);
+//       r.style.setProperty("--hsl-value", hslValue - 10);
+//     });
+//   });
+// }
+
+function attachEventListener() {
+  const cells = document.querySelectorAll(".box-class");
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", function (e) {
+      let r = document.querySelector(":root");
+      let hslValue = getComputedStyle(r).getPropertyValue("--hsl-value");
+      let cellClass = cell.classList[1];
+      console.log(cellClass);
+      let newHsl = hslValue - 50;
+      cell.style.backgroundColor = `hsl(217, 0%, ${newHsl}%)`;
+    });
+  });
+}
 
 function buildGrid(rows, cols) {
-  box.addEventListener(
-    "mouseover",
-    function (e) {
-      // let r = document.querySelector(":root");
-      // let hslValue = getComputedStyle(r).getPropertyValue("--hsl-value");
-      // console.log(hslValue);
-      // r.style.setProperty("--hsl-value", hslValue - 50);
-      console.log("HELLO WORLD");
-    },
-    false
-  );
   gridDiv.style.setProperty("--grid-rows", rows);
   gridDiv.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     box.classList.remove(`box-div-${c}`);
     box.classList.add(`box-div-${c + 1}`);
-    gridDiv.appendChild(box.cloneNode(true));
+    gridDiv.appendChild(box.cloneNode(true)); // use cloneNode for appending element multiple times with a loop (doesn't copy event listeners of the element)
   }
+  attachEventListener();
 }
 
 buildGrid(16, 16);
